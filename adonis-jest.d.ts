83766be15b1e3203470e27d39a49ab83ea64b5cf
Michaël Zasso <targos@protonmail.com>
2021-02-19 13:44:21 +0100

    chore: remove files from previous implementation

--- adonis-jest.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Setup an application for testing.
- *
- * @param appRoot Root directory of the application. Usually `__dirname`.
- */
-export declare function setupTestApplication(appRoot: string): void;
